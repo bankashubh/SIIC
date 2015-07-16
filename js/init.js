ef50d833c9fa87e1ca9f77f222dc9173c5c9970c
Kunal Bhuwalka <kunal.bhuwalka@hotmail.com>
2015-07-17 02:09:00 +0530

    Added input requirements to navigation
    Disabled navigation without filling the forms completely.

--- js/init.js
+++ js/init.js
@@ -10,6 +10,7 @@
 	 
 		initNavigation();
 		
+		
 	 
     });
 	
@@ -21,18 +22,34 @@
 		
 		//Disable all the tabs
 		$("#registration-card-container .tab:nth-child(n+2)").addClass("disabled");
-		//Disable the back button
+		//Disable the back button and next button
 		$("#back-btn").addClass("disabled");
-		//Hide submit Button
-		//$("#registration-submit-btn").hide();
+		$("#next-btn").addClass("disabled");
 		
-		$("#next-btn").on("click", function(){
+		//Prevent submit on enter
+		preventEnter();
+		
+		console.log($("#"+$pages[$current]+" input"));
+		$("#"+$pages[$current]+" input").on("keyup change mouseenter", checkForEmptyFields);
+		
+		$("#next-btn").on("click mouseenter", function(){
+			checkForEmptyFields();
+			
+			if($(this).hasClass("disabled")){
+				return;
+			}
+			
 			$("#"+$pages[$current]+"-tab").addClass("disabled");
 			$current++;
 			
 			$("#"+$pages[$next]+"-tab").removeClass("disabled");
 			$('ul.tabs').tabs('select_tab', $pages[$next]);
 			$next++;
+			
+			$("#"+$pages[$current]+" input")
+			.on("keyup change mouseenter", checkForEmptyFields);
+			$("#"+$pages[$current]+" select")
+			.on("keyup change mouseenter", checkForEmptyFields);
 			
 			checkForPageNumber($current);
 		});
@@ -49,26 +66,46 @@
 			
 			checkForPageNumber($current);
 		});
+		
+		$("#registration-submit-btn").on("click", function(){
+			
+			$(this).submit();
+		});
 	}
 
 	
 	function checkForPageNumber($current){
-		if($current == 0){
-				//Disable the back button
+		if($current == 0)
 				$("#back-btn").addClass("disabled");
-			}
-			else {
-				//Enable the back button
+			else
 				$("#back-btn").removeClass("disabled");
-			}
 			
-			if( $current == 2 ){
+			if( $current == $pages.length-1 )
 				$("#next-btn").hide();
-				//$("#registration-submit-btn").show();
-			}
 			
-			if(($("#next-btn").is(":hidden")) && $current < 2){
+			if(($("#next-btn").is(":hidden")) && $current < $pages.length-1)
 				$("#next-btn").show();
-				//$("#registration-submit-btn").hide();
-			}
 	}
+	
+	function preventEnter(){
+		$(window).keydown(function(event){
+			if(event.keyCode == 13) {
+				$form = $("#registration-form input, #registration-form select");
+				$form.each(function(){
+					if(!$(this).val())
+						event.preventDefault();
+				});
+				return false;
+			}
+		});
+	}
+	
+	var checkForEmptyFields = (function(){
+		$("#"+$pages[$current]+" input, #"+$pages[$current]+" select").each(function(){
+				if(!$(this).val()){
+					$("#next-btn, #registration-submit-btn").addClass("disabled");
+					return false;
+				}	
+				$("#next-btn, #registration-submit-btn").removeClass("disabled");
+			});
+	});
